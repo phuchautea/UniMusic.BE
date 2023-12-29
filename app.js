eecfd705853be66cb6c510bcb436372c706c4c53
Hoang Phuc Hau <phuchau.tea@gmail.com>
2023-12-29 22:50:52 +0700

    ✅ Fix Path

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const routes = require("../routes");
-const mongoConfig = require("../config/mongodb.config");
+const routes = require("./routes");
+const mongoConfig = require("./config/mongodb.config");
 
 const app = express();
 const PORT = 3000;
