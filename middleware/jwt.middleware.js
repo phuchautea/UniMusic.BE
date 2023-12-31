9680dfd348199b39e6a5a044aecb682dbf085f8f
Hoang Phuc Hau <phuchau.tea@gmail.com>
2023-12-31 18:11:19 +0700

    🚧 Adjust Bearer Token

--- middleware/jwt.middleware.js
+++ middleware/jwt.middleware.js
@@ -4,7 +4,29 @@ const { secretKey } = require("../config/jwt.config");
 const authenticateJWT =
 	(options = {}) =>
 	(req, res, next) => {
-		const token = req.header("Authorization");
+		const authHeader = req.header("Authorization");
+
+        if (!authHeader) {
+            if (options.optional) {
+                // No token provided, but authentication is optional
+                return next();
+            } else {
+                return res
+                    .status(401)
+                    .json({ message: "Unauthorized - Missing token" });
+            }
+        }
+
+        // Check if the Authorization header is in the format "Bearer <token>"
+        const parts = authHeader.split(' ');
+        if (parts.length !== 2 || parts[0] !== 'Bearer') {
+            return res
+                .status(401)
+                .json({ message: "Unauthorized - Invalid token format" });
+        }
+
+        const token = parts[1];
+		// const token = req.header("Authorization");
 
 		if (!token) {
 			if (options.optional) {
