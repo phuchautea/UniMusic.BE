506e99fac13bc686bff40430392951d0e5207787
Hoang Phuc Hau <phuchau.tea@gmail.com>
2024-02-24 08:39:33 +0700

    user model

--- models/user.model.js
+++ models/user.model.js
@@ -1,19 +1,19 @@
 const mongoose = require("mongoose");
 const Role = require('./role.model');
-const Playlist = require('./playlist.model');
-const RecentlyPlayed = require('./recentlyPlayed.model');
-const Payment = require('./payment.model');
-const Subscription = require('./subscription.model');
+// const Playlist = require('./playlist.model');
+// const RecentlyPlayed = require('./recentlyPlayed.model');
+// const Payment = require('./payment.model');
+// const Subscription = require('./subscription.model');
 
 const userSchema = new mongoose.Schema({
   username: String,
   password: String,
   email: String,
   role: { type: mongoose.Schema.Types.ObjectId, ref: Role },
-  playlists: [{ type: mongoose.Schema.Types.ObjectId, ref: Playlist }],
-  recentlyPlayed: [{ type: mongoose.Schema.Types.ObjectId, ref: RecentlyPlayed }],
-  payments: [{ type: mongoose.Schema.Types.ObjectId, ref: Payment }],
-  subscriptions: [{ type: mongoose.Schema.Types.ObjectId, ref: Subscription }],
+  // playlists: [{ type: mongoose.Schema.Types.ObjectId, ref: Playlist }],
+  // recentlyPlayed: [{ type: mongoose.Schema.Types.ObjectId, ref: RecentlyPlayed }],
+  // payments: [{ type: mongoose.Schema.Types.ObjectId, ref: Payment }],
+  // subscriptions: [{ type: mongoose.Schema.Types.ObjectId, ref: Subscription }],
 });
 
 const userModel = mongoose.model("User", userSchema);
