71bd9b69e510d58d7c15828a8a024c7341b6b041
Hoang Phuc Hau <phuchau.tea@gmail.com>
2024-02-29 06:45:17 +0700

    routes

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const adsRoutes = require('./ads.routes');
 const authRoutes = require('./auth.routes');
-
+const songRoutes = require('./song.routes');
+const artistRoutes = require('./artist.routes');
+const albumRoutes = require('./album.routes');
+const genreRoutes = require('./genre.routes');
+const s3Routes = require('./s3.routes');
 
 
 const router = express.Router();
 
 router.use('/api', adsRoutes);
 router.use('/api', authRoutes);
+router.use('/api', songRoutes);
+router.use('/api', artistRoutes);
+router.use('/api', albumRoutes);
+router.use('/api', genreRoutes);
+router.use('/api', s3Routes);
+
 
 module.exports = router;
